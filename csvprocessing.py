5b543ff06616d40121645f457a6a78034071e542
laya <layadelcarmen@gmail.com>
2021-11-05 00:25:44 +0100

    add function calculate_percentile_for_column
    - calculate the percentile for the specified column in a Dataframe
    - add corresponding test

--- csvprocessing.py
+++ csvprocessing.py
@@ -8,3 +8,10 @@ def load_csv_data(file):
     data = pd.read_csv(file, low_memory=False)
     return data
 
+
+def calculate_percentile_for_column(df, column, ref_percentile):
+    '''
+    Calculate the percentile for the specified column in a Dataframe
+    '''    
+    return df[column].quantile(ref_percentile)
+
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 import pytest
 
-from csvprocessing import load_csv_data
+from csvprocessing import load_csv_data, calculate_percentile_for_column
 
 from unittest.mock import patch
 
@@ -23,3 +23,11 @@ def _load_test_data():
 @patch('csvprocessing.pd.read_csv')
 def test_load_csv_data(load_csv_data):   
     pass
+
+
+@pytest.mark.parametrize("column, ref_percentile, val_expected", [("feature_1", 0.9, 6.2)])
+def test_percentil_for_column(column, ref_percentile, val_expected):   
+    data = _load_test_data()
+    expected = val_expected
+    result = calculate_percentile_for_column(data, column, ref_percentile)
+    assert expected == result
