240a6a01069dddd95f505ba4c0f9ddd9926e3f81
laya <layadelcarmen@gmail.com>
2021-11-06 01:01:37 +0100

    refactor main to expose results in the comand line
    - create function pipeline to return records over a given percentile
    
    - call functions to orchestrate the percentile
    - use a python scrito to touput the results

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
-from flask import jsonify, request
-
-from pyms.flask.app import Microservice
+import sys, getopt
 
 from csvprocessing import load_csv_data, calculate_percentile_for_column, get_records_above
-
-ms = Microservice() 
-app = ms.create_app() 
-
-
-@app.route("/") 
-def example():
-    return jsonify({"main": "Hello, Yellow taxi"})
 
 
 # file_example = 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-03.csv'
 
-@app.route("/percentile") 
-def pipeline_percentile():
-    file = request.args.get('file')
-    column = request.args.get('column')
-    percentil = float(request.args.get('age'))
+
+def _print_help():
+    _help = '''
+    USAGE: script.py <inputfile> <column_name> <percentile>
+
+    Example: 
+    
+    file: 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2020-03.csv
+
+    column: trip_distance
+
+    percentile: 0.9
+
+    '''
+    print(_help)
+
+
+def pipeline_percentile(file, column, percentil):
     data = load_csv_data(file)
-    val_percentile = calculate_percentile_for_column(data, column, percentil)
-    return get_records_above(data, column, val_percentile)
+    if check_data_type(data, column, ["float64"]):
+        val_percentile = calculate_percentile_for_column(data, column, percentil)
+        print(get_records_above(data, column, val_percentile))
+
+
+def main(argv):
+
+    try:
+        opts, args = getopt.getopt(argv,"h")
+    except getopt.GetoptError:
+        _print_help()
+        sys.exit(2)
+   
+    if len(args) == 2 and args[1] == 'h':
+        _print_help()
+        sys.exit()
+    elif len(args) == 4:       
+        file = args[1] 
+        column =  args[2]
+        percentil = float(args[3])
+    else:
+        _print_help()
+        sys.exit()
+
+    pipeline_percentile(file, column, percentil)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    main(sys.argv)
